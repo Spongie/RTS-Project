577e66a526a27e68e54eac829b874d0a5a817379
Philip Persson <kalllez123@hotmail.se>
2013-04-12 14:46:34 +0200

    Skapat 3D testProgram
    Skapat ett program som ritar ut 2 modeller(1 går att styra) och med
    flyttbar camera.

---
+++ XNA3DTesting/XNA3DTesting/Camera.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace XNA3DTesting
+{
+    class Camera
+    {
+        public Vector3 position;
+        public Vector3 lookAt;
+        public Vector3 up;
+        KeyboardState keyState;
+
+        public Camera(Vector3 pos, Vector3 look)
+        {
+            position = pos;
+            lookAt = look;
+            up = Vector3.Up;
+        }
+
+        public void UpdateCamera()
+        {
+            keyState = Keyboard.GetState();
+
+            if (keyState.IsKeyDown(Keys.A))
+            {
+                position.X--;
+                lookAt.X--;
+            }
+            if (keyState.IsKeyDown(Keys.D))
+            {
+                position.X++;
+                lookAt.X++;
+            }
+
+            if (keyState.IsKeyDown(Keys.W))
+            {
+                position.Z--;
+                lookAt.Z--;
+            }
+            if (keyState.IsKeyDown(Keys.S))
+            {
+                position.Z++;
+                lookAt.Z++;
+            }
+            if (keyState.IsKeyDown(Keys.Q))
+            {
+                position.Y--;
+                lookAt.Y--;
+            }
+            if (keyState.IsKeyDown(Keys.E))
+            {
+                position.Y++;
+                lookAt.Y++;
+            }
+            if (keyState.IsKeyDown(Keys.D1))
+            {
+                lookAt.X -= 5;
+                lookAt.Z -= 5;
+            }
+            if (keyState.IsKeyDown(Keys.D2))
+            {
+                lookAt.Z += 5;
+                lookAt.X += 5;
+            }
+
+        }
+    }
+}
---
+++ XNA3DTesting/XNA3DTesting/thing.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace XNA3DTesting
+{
+    class thing
+    {
+        Model model;
+        Vector3 position;
+        Vector3 scale;
+        Vector3 rotation;
+        KeyboardState keystate;
+
+        public thing(Model mod, Vector3 pos)
+        {
+            model = mod;
+            position = pos;
+            scale = new Vector3(1, 1, 1);
+            rotation = new Vector3(0, 0, 0);
+        }
+
+        public void Update()
+        {
+            keystate = Keyboard.GetState();
+
+            if (keystate.IsKeyDown(Keys.Left))
+                position.X--;
+            if (keystate.IsKeyDown(Keys.Right))
+                position.X++;
+            if (keystate.IsKeyDown(Keys.Down))
+                position.Z++;
+            if (keystate.IsKeyDown(Keys.Up))
+                position.Z--;
+            if (keystate.IsKeyDown(Keys.Space))
+                rotation.Y+= 0.1f;
+        }
+
+        public void Draw(Matrix view, Matrix projection)
+        {
+            Matrix[] transforms = new Matrix[model.Bones.Count];
+            model.CopyAbsoluteBoneTransformsTo(transforms);
+
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.View = view;
+                    effect.World = transforms[mesh.ParentBone.Index] * Matrix.CreateRotationY(rotation.Y) * Matrix.CreateTranslation(position);
+                    effect.Projection = projection;
+                    effect.EnableDefaultLighting();
+                }
+                mesh.Draw();
+            }
+        }
+    }
+}
