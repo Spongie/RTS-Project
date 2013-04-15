c99ac8214cd28d7ea0cf000c7a8f69a174611de3
Tim Tuvestam <maus_tim@hotmail.com>
2013-04-15 13:57:44 +0200

    MovingObject

---
+++ XNA_RTSProject/XNA_RTSProject/MovingObject.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace XNA_RTSProject
+{
+    class MovingObject:BaseObject
+    {
+        #region classValues
+        private Vector3 targetPosition;
+        private float speed;
+        private float selectionPriority;
+        private float timeToTrain;
+        private float cost;
+        private bool isStealthed;
+        private float damage;
+        private float timeBetweenAttacks;
+        private float timeSinceLastAttack;
+        #endregion
+
+        #region properties
+
+        public Vector3 TargetPosition
+        {
+            get { return targetPosition; }
+            set { targetPosition = value; }
+        }
+
+        public float Damage
+        {
+            get { return damage; }
+            set { damage = value; }
+        }
+
+        public bool IsStealthed
+        {
+            get { return isStealthed; }
+            set { isStealthed = value; }
+        }
+
+        public float SelectionPriority
+        {
+            get { return selectionPriority; }
+        }
+
+        #endregion
+
+        public override void Update(GameTime gameTime)
+        {
+
+        }
+
+        public virtual void Draw(Matrix view, Matrix projction)
+        {
+
+        }
+    }
+}
