ddeb6580bc53629aceed02a6f45d43aaa3ba561b
Philip Persson <kalllez123@hotmail.se>
2013-04-15 13:52:13 +0200

    Started TestMatch
    Waiting for some code to contunie

--- XNA_RTSProject/XNA_RTSProject/TestMatch.cs
+++ XNA_RTSProject/XNA_RTSProject/TestMatch.cs
@@ -14,6 +14,28 @@ namespace XNA_RTSProject
 {
     class TestMatch
     {
+        private Matrix view;
+        private Matrix projection;
+        private MovableObject testObject;
+        private float aspectRatio;
+        Camera camera;
 
+        public TestMatch(Model testModel, float aspRatio)
+        {
+            aspectRatio = aspRatio;
+            projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45.0f), aspectRatio, 1f, 1000f);
+            //testObject = new testObject(we need more stuffs
+        }
+
+        public void Update(GameTime gameTime)
+        {
+           // view = Matrix.CreateLookAt( Waiting for Camera to be done
+            testObject.Update(gameTime);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            testObject.Draw(view, projection);
+        }
     }
 }
