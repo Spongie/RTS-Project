15f763b37b42f69ac0a221cf012d3a98811529d1
Philip Persson <kalllez123@hotmail.se>
2013-04-17 00:23:52 +0200

    TestProject för 3D Animering

---
+++ XNA3DTestingAnimation/XNA3DTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("XNA3DTesting")]
+[assembly: AssemblyProduct("XNA3DTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type. Only Windows
+// assemblies support COM.
+[assembly: ComVisible(false)]
+
+// On Windows, the following GUID is for the ID of the typelib if this
+// project is exposed to COM. On other platforms, it unique identifies the
+// title storage container when deploying this assembly to the device.
+[assembly: Guid("3e6c4952-4a8b-439e-9d8f-c1b3a3743d5e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ XNA3DTestingAnimation/XNA3DTesting/thing.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using SkinnedModel;
+
+namespace XNA3DTesting
+{
+    class thing
+    {
+        Model model;
+        Vector3 position;
+        Vector3 scale;
+        Vector3 rotation;
+        KeyboardState keystate;
+        AnimationPlayer animPlayer;
+        
+        public thing(Model mod, Vector3 pos)
+        {
+            model = mod;
+            position = pos;
+            scale = new Vector3(1, 1, 1);
+            rotation = new Vector3(0, 0, 0);
+            SkinningData skData = model.Tag as SkinningData;
+            animPlayer = new AnimationPlayer(skData);
+            AnimationClip clip = skData.AnimationClips["Walking"];
+            animPlayer.StartClip(clip);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            keystate = Keyboard.GetState();
+
+            if (keystate.IsKeyDown(Keys.Left))
+                position.X--;
+            if (keystate.IsKeyDown(Keys.Right))
+                position.X++;
+            if (keystate.IsKeyDown(Keys.Down))
+                position.Z++;
+            if (keystate.IsKeyDown(Keys.Up))
+                position.Z--;
+            if (keystate.IsKeyDown(Keys.Space))
+                rotation.Y+= 0.1f;
+
+                 animPlayer.Update(gameTime.ElapsedGameTime, true, Matrix.Identity);
+
+            
+        }
+
+        public void Draw(Matrix view, Matrix projection)
+        {
+            Matrix[] bones = animPlayer.GetSkinTransforms();
+
+            Matrix[] transforms = new Matrix[model.Bones.Count];
+            model.CopyAbsoluteBoneTransformsTo(transforms);
+
+            
+
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (SkinnedEffect effect in mesh.Effects)
+                {
+                    effect.SetBoneTransforms(bones);
+
+                    effect.View = view;
+                    effect.World = transforms[mesh.ParentBone.Index] * Matrix.CreateRotationY(rotation.Y) * Matrix.CreateTranslation(position);
+                    effect.Projection = projection;
+                    effect.EnableDefaultLighting();
+                }
+                mesh.Draw();
+            }
+        }
+    }
+}
