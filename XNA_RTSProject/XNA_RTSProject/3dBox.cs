a2ccc14a42ceb18c0ac05d8212fb113e3f3e954c
Oskar <Ystkund@hotmail.com>
2013-04-17 11:33:03 +0200

    OSKAR BITCHES! D:<

--- XNA_RTSProject/XNA_RTSProject/3dBox.cs
+++ XNA_RTSProject/XNA_RTSProject/3dBox.cs
@@ -2,10 +2,66 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace XNA_RTSProject
 {
     class _3dBox
     {
+        private Vector3 position;
+        private int height;
+        private int lenght;
+        private int depth;
+
+        public _3dBox(Vector3 position, int height, int lenght, int depth)
+        {
+            this.position = position;
+            this.height = height;
+            this.lenght = lenght;
+            this.depth = depth;
+        }
+        public Vector3 Position
+        {
+            get { return position; }
+            set
+            {
+                if (value != null)
+                    position = value;
+            }
+        }
+        public int Height
+        {
+            get { return height; }
+            set
+            {
+                if (value != null)
+                    height = value;
+            }
+        }
+
+        public int Lenght
+        {
+            get { return lenght; }
+            set
+            {
+                if (value != null)
+                    lenght = value;
+            }
+        }
+        public int Depth
+        {
+            get { return depth; }
+            set
+            {
+                if (value != null)
+                    depth = value;
+            }
+        }
     }
 }
