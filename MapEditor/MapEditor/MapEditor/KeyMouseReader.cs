7ae4469c8bc521d29718ad928b8670aa06976d11
Philip Persson <kalllez123@hotmail.se>
2013-04-17 11:08:53 +0200

    Added Long planning and

---
+++ MapEditor/MapEditor/MapEditor/KeyMouseReader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+
+static class KeyMouseReader
+{
+	public static KeyboardState keyState, oldKeyState = Keyboard.GetState();
+	public static MouseState mouseState, oldMouseState = Mouse.GetState();
+	public static bool KeyPressed(Keys key) {
+		return keyState.IsKeyDown(key) && oldKeyState.IsKeyUp(key);
+	}
+
+	//Should be called at beginning of Update in Game
+	public static void Update() {
+		oldKeyState = keyState;
+		keyState = Keyboard.GetState();
+		oldMouseState = mouseState;
+		mouseState = Mouse.GetState();
+	}
+}
